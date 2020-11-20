0ca1224f31bffdcdddfbc2659160bdf7dce67c04
Silvan Verhoeven <SilvanVerhoeven@github.com>
2020-11-20 22:23:55 +0100

    Implement custom button definitions

--- src/executor.ts
+++ src/executor.ts
@@ -6,6 +6,12 @@ function log(message: string, lineToOverride?: number): void
 function send(message: string): void
 `;
 
+interface IButton {
+  caption?: string,
+  id?: string,
+  onclick?: () => void
+}
+
 function log(message: string, lineToOverride?: number) {
   if (typeof lineToOverride === "number") {
     overrideToOutlog(lineToOverride, message);
@@ -18,9 +24,56 @@ function send(message: string) {
   WalkieTalkie.send(message);
 }
 
+function createButtons(buttons: IButton[]) {
+  document.getElementById("btn-container").innerHTML = "";
+  buttons.forEach(button => {
+    createButton(button);
+  });
+}
+
+function createButton(button: IButton) {
+  if (typeof button.caption === "undefined" || button.caption.length === 0) return;
+  const btnContainer = document.getElementById("btn-container");
+  btnContainer.innerHTML += `<button id="${(button.id ? button.id : "")}" class="btn btn-primary" type="button" onclick="${button.onclick}()">${button.caption}</button>`
+}
+
 export const initialContent = `
+// === Button definitions =====================
+
+var buttons = [
+  {
+    caption: "A",
+    onclick: "onPressA",
+    id: "btnA"
+  },
+  {
+    caption: "B",
+    onclick: "onPressB",
+    id: "btnB"
+  }
+  /* New button:
+     {
+       caption: string,
+       onclick: function name (add to routes below),
+       id: string (optional)
+     }
+  */
+];
+
+// === Route button functions =================
+
+// simply add 'window.' before the onclick-function
+window.onPressA = onPressA;
+window.onPressB = onPressB;
+
+// === Your code =============================
+
 function onPressA() {
   log("A was pressed")
+}
+
+function onPressB() {
+  log("B was pressed")
 }
 
 function onReceive(msg) {
@@ -28,7 +81,7 @@ function onReceive(msg) {
 }
 
 function onSubmit(input) {
-  // do smth
+  // do smth when pressing enter in text input
 }
 `.trim();
 
@@ -36,16 +89,15 @@ WalkieTalkie.listen((msg) => (window as any).handlers.onReceive(msg));
 
 export default function execute(code: string) {
   eval(`
-window.buttons = {}
-window.buttons.onPressA = typeof onPressA !== "undefined" ? onPressA : () => alert("Specify onPressA()");
-window.buttons.onPressB = typeof onPressB !== "undefined" ? onPressB : () => alert("Specify onPressB()");
-window.buttons.onPressC = typeof onPressC !== "undefined" ? onPressC : () => alert("Specify onPressC()");
-window.buttons.onPressD = typeof onPressD !== "undefined" ? onPressD : () => alert("Specify onPressD()");
+    window.handlers = {}
+    window.handlers.onReceive = typeof onReceive !== "undefined" ? onReceive : undefined;
+    window.handlers.onSubmit = typeof onSubmit !== "undefined" ? onSubmit : () => alert("Specify onSubmit()");
 
-window.handlers = {}
-window.handlers.onReceive = typeof onReceive !== "undefined" ? onReceive : undefined;
-window.handlers.onSubmit = typeof onSubmit !== "undefined" ? onSubmit : () => alert("Specify onSubmit()");
+    var buttons = [];
 
-${code}
+    ${code}
+
+    createButtons(buttons);
+
   `);
 }
